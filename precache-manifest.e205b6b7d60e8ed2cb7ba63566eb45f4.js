392d846cefbc488d1fd8ab7fb2a848bd390284f5
Kelsey Steeves <kelsey.steeves@youi.tv>
2019-11-06 12:11:28 -0500

    Updates

---
+++ precache-manifest.e205b6b7d60e8ed2cb7ba63566eb45f4.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1905cbb9c3d889b3346811982c24d0",
+    "url": "/EmojiWorkflow/index.html"
+  },
+  {
+    "revision": "139870980534c042f8a0",
+    "url": "/EmojiWorkflow/static/css/2.5d2c437e.chunk.css"
+  },
+  {
+    "revision": "0f79e31e29aa3f6cfe9d",
+    "url": "/EmojiWorkflow/static/css/main.d83df609.chunk.css"
+  },
+  {
+    "revision": "139870980534c042f8a0",
+    "url": "/EmojiWorkflow/static/js/2.092b4398.chunk.js"
+  },
+  {
+    "revision": "0f79e31e29aa3f6cfe9d",
+    "url": "/EmojiWorkflow/static/js/main.7292dc20.chunk.js"
+  },
+  {
+    "revision": "f885500fbaa090bf4867",
+    "url": "/EmojiWorkflow/static/js/runtime-main.db726bb7.js"
+  },
+  {
+    "revision": "6585e257a3996ee676d35f045b20d0c2",
+    "url": "/EmojiWorkflow/static/media/blank_eyes.6585e257.png"
+  },
+  {
+    "url": "/EmojiWorkflow/static/media/checkbcd1a436590360ed53ac2f46c76e43e4.bcd1a436.png"
+  },
+  {
+    "revision": "3c4c32b9a67272220a01147cef7d8f65",
+    "url": "/EmojiWorkflow/static/media/nice_blank_eyes.3c4c32b9.png"
+  }
+]);
