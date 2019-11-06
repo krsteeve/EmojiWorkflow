1d5a83c58de1d1d93a091087321eb095766ecf1e
Kelsey Steeves <kelsey.steeves@youi.tv>
2019-11-06 13:51:30 -0500

    Updates

--- precache-manifest.beec0e1ef52254f7462c3af88735ca44.js
+++ precache-manifest.beec0e1ef52254f7462c3af88735ca44.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "95f0c25d00761dc60535573326a12de6",
+    "revision": "35351be3ece8dbf7fdbf18b9011a522f",
     "url": "/EmojiWorkflow/index.html"
   },
   {
-    "revision": "139870980534c042f8a0",
+    "revision": "2d7b0d15131673d15f42",
     "url": "/EmojiWorkflow/static/css/2.5d2c437e.chunk.css"
   },
   {
-    "revision": "d845e458e0bc5a86a832",
+    "revision": "00b2767b8e8179d9ffa5",
     "url": "/EmojiWorkflow/static/css/main.d83df609.chunk.css"
   },
   {
-    "revision": "139870980534c042f8a0",
-    "url": "/EmojiWorkflow/static/js/2.092b4398.chunk.js"
+    "revision": "2d7b0d15131673d15f42",
+    "url": "/EmojiWorkflow/static/js/2.f50f68ce.chunk.js"
   },
   {
-    "revision": "d845e458e0bc5a86a832",
-    "url": "/EmojiWorkflow/static/js/main.35a582f4.chunk.js"
+    "revision": "00b2767b8e8179d9ffa5",
+    "url": "/EmojiWorkflow/static/js/main.3078ce9d.chunk.js"
   },
   {
     "revision": "f885500fbaa090bf4867",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "6585e257a3996ee676d35f045b20d0c2",
     "url": "/EmojiWorkflow/static/media/blank_eyes.6585e257.png"
+  },
+  {
+    "revision": "d7309f235227d6f3679a90b2aea874ee",
+    "url": "/EmojiWorkflow/static/media/blank_joy.d7309f23.png"
   },
   {
     "url": "/EmojiWorkflow/static/media/checkbcd1a436590360ed53ac2f46c76e43e4.bcd1a436.png"
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EmojiWorkflow/precache-manifest.1a8f570f32c264f77fcc5aaf289ee68f.js"
+  "/EmojiWorkflow/precache-manifest.beec0e1ef52254f7462c3af88735ca44.js"
 );
 
 self.addEventListener('message', (event) => {
