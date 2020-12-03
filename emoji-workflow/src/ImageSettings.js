6c6987b052922d29a9330ed39938dacfca5ef0a2
Kelsey Steeves <kelsey.steeves@youi.tv>
2020-12-03 14:47:10 -0500

    add loading indicator for URLs, error indicator
    also updated packages to address vunerabilities

--- emoji-workflow/src/ImageSettings.js
+++ emoji-workflow/src/ImageSettings.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Settings from './Settings';
+import Loader from "react-spinners/GridLoader";
 
 export default class ImageSettings extends Component {
 
   state = {
     image: {},
     liveSettings: {},
-    imageSourceType: "file"
+    imageSourceType: "file",
+    loading: false,
+    error: false,
   }
 
   componentDidMount() {
@@ -24,18 +27,26 @@ export default class ImageSettings extends Component {
   onImageSourceAvailable = (src) => {
     var image = new Image();
 
-    image.crossOrigin = "";
-    image.src = src;
-
     image.onload = () => {
       this.setState(state => (
-        { image: { src: src, aspectRatio: image.width / image.height } }),
+        { image: { src: src, aspectRatio: image.width / image.height, width: image.width }, loading: false }),
         () => this.props.onChange(this.state.image, this.state.liveSettings)
       );
     };
+
+    image.onerror = () => {
+      this.setState({ loading: false, error: true });
+    };
+
+    this.setState({ loading: true });
+
+    image.crossOrigin = "";
+    image.src = src;
   }
 
   onSelectImage = (e) => {
+    this.setState({ error: false });
+
     if (this.state.imageSourceType === "file") {
       if (e.target.files && e.target.files.length > 0) {
         const reader = new FileReader()
@@ -49,7 +60,7 @@ export default class ImageSettings extends Component {
         reader.readAsDataURL(e.target.files[0])
       }
     }
-    else {
+    else if (e.target.value.length > 0) {
       this.onImageSourceAvailable("https://cors-anywhere.herokuapp.com/" + e.target.value);
     }
   }
@@ -66,16 +77,39 @@ export default class ImageSettings extends Component {
   }
 
   render() {
+    const imageWidth = this.state.image ? this.state.image.aspectRatio * 100 : 100;
+    const loaderSize = Math.min(100, imageWidth) * 0.25;
+    const loaderMargin = (Math.min(100, imageWidth) * 0.08) + Math.max((imageWidth - 100) / 2.0, 0);
     return (
       <div className="AppSmall" style={{ flexWrap: "wrap", paddingLeft: 30 }}>
-        <div className="AppSmall">
+        <div className="AppSmall" style={{ paddingRight: 5 }}>
           <div style={{ display: "flex", flexDirection: "column" }}>
             <div style={{ justifyContent: "center" }}>{this.state.liveSettings.imageTitle}:</div>
-            <img src={this.state.image ? this.state.image.src : null} style={this.state.image ? { height: 100, border: "1px solid white" } : { height: 100, width: 100, border: "1px solid white" }} alt="" />
-            <div>{this.state.imageSourceType == "url" ? "URL: " : ""} <input type={this.state.imageSourceType} onChange={this.onSelectImage} /></div>
+            <div style={{ display: "flex" }}>
+              <img
+                src={this.state.image ? this.state.image.src : null}
+                style={{
+                  height: 100,
+                  width: imageWidth,
+                  border: "1px solid white"
+                }}
+                alt=""
+              />
+              <div style={{ position: "absolute", display: "flex", alignSelf: "center", marginLeft: loaderMargin }}>
+                <Loader
+                  size={loaderSize}
+                  color={"white"}
+                  loading={this.state.loading}
+                />
+              </div>
+            </div>
+            <div>
+              {this.state.imageSourceType === "url" ? "URL: " : ""}
+              <input type={this.state.imageSourceType} onChange={this.onSelectImage} style={{ border: this.state.error ? "4px solid red" : "" }} />
+            </div>
             <div style={{ alignItems: "center" }}>
-              <button onClick={() => { this.setState({ imageSourceType: "file" }) }}>File</button>&nbsp;
-              <button onClick={() => { this.setState({ imageSourceType: "url" }) }}>Url</button>
+              <button onClick={() => { this.setState({ imageSourceType: "file", error: false }) }}>File</button>&nbsp;
+              <button onClick={() => { this.setState({ imageSourceType: "url", error: false }) }}>Url</button>
             </div>
           </div>
         </div>
